6c6b2e7f5ed96b4ce76e57049da566ffa3fceeee
Jasmin <Jasmin.wonnacott@outlook.com>
2025-06-06 12:09:37 +0100

    Update multiple files with logic and config changes

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-
+import json
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 import os
 import asyncio
 import aiohttp
-import psycopg2
+import asyncpg
 
 
 #  load environment variables
 load_dotenv()
 
-
-#  Connect to the database
-conn = psycopg2.connect (
-    dbname=os.getenv("db_name_sec"),
-    user= os.getenv("user_sec"),
-    password= os.getenv("postgres_key"),
-    host="localhost"
-)
-
-#  create a cursor object
-cur = conn.cursor()
-
 async def get_api(session, params):
     base_url = "https://api.exchange.coinbase.com/products/BTC-USD/candles"
-
     async with session.get(base_url, params=params) as response:
-        return await response.json()
+        if response.status !=200:
+            print(f"Error bad response status {response.status}")
+            return []
+        try:
+            data = await response.json()
+            if not isinstance(data, list):
+                print(f"Error data is not in a list")
+                return []
+            return data
+        except json.JSONDecodeError:
+            print(f"Error parsing to JSON")
+            return []
 
 
 def api_time_requests():
@@ -43,6 +41,13 @@ def api_time_requests():
 
 
 async def main():
+    pool = await asyncpg.create_pool(
+        database=os.getenv("db_name"),
+        user=os.getenv("user_sec"),
+        password=os.getenv("postgres_key"),
+        host=os.getenv("local_host")
+    )
+
     async with aiohttp.ClientSession() as session:
          time_records = api_time_requests()
 
@@ -61,21 +66,16 @@ async def main():
 
              for candle in data:
                  timestamp = datetime.fromtimestamp(candle[0], tz=timezone.utc)
-                 cur.execute(
+                 async with pool.acquire() as conn:
+                     await conn.execute(
                     """
                     INSERT INTO coinbase_data (time, low, high, open, close, volume)
-                    VALUES (%s, %s, %s, %s, %s, %s)
+                    VALUES ($1, $2, $3, $4, $5, $6)
                     """,
-                    (timestamp, candle[1], candle[2], candle[3], candle[4], candle[5])
+                    timestamp, candle[1], candle[2], candle[3], candle[4], candle[5]
                 )
 
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-
-
+    await pool.close()
 asyncio.run(main())
 
 
